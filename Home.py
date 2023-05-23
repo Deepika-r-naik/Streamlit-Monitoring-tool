821061094aaf5c9bbf4d29b5e288a15980c77826
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2023-05-23 22:49:32 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,12 +2,11 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 import plotly.graph_objects as go
+import altair as alt
 
 import numpy as np  # np mean, np random
 
 st.set_page_config(page_title="Observability Monitoring Tool", page_icon=":bar_chart:", layout="wide")
-
-
 
 
 # Add menu bar options
@@ -16,7 +15,7 @@ st.markdown("---")
 
 menu_options = ["Home", "Dashboard", "Search", "Settings", "Contact"]
 
-# Create a div container for the menu options
+#  menu options
 menu_container = st.empty()
 with menu_container:
     st.markdown(
@@ -29,7 +28,7 @@ with menu_container:
         if col.button(menu_options[i]):
             if menu_options[i] == "Home":
                 st.title("Home Page")
-                # Add your home page content here
+                
 
             elif menu_options[i] == "Dashboard":
                 st.title("Dashboard Page")
@@ -52,35 +51,40 @@ with menu_container:
 
 st.markdown("---")
 
-
-
-
 df = pd.read_csv("data.csv").rename(columns=str.lower)
-
-
-
 
 #charts for observability
- # create two columns for charts
 
-df = pd.read_csv("data.csv").rename(columns=str.lower)
+
+# Create columns for arranging the charts
 fig_col1, fig_col2 = st.columns(2)
-fig_col3, fig_col4 = st.columns(2)
+
+# Display the third chart (Network Performance per Cluster)
 with fig_col1:
-            st.markdown("### First Chart")
-            fig = px.density_heatmap(
-                data_frame=df, y="clusters", x="availability"
-            )
-            st.write(fig)
-            
+    st.markdown("### Network Performance per Cluster")
+    fig1 = px.density_heatmap(data_frame=df, y="clusters", x="network performances")
+    st.plotly_chart(fig1)
+
+# Display the fourth chart (Number of Incidents per Cluster)
 with fig_col2:
-            st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame=df, x="availability")
-            st.write(fig2) 
+    st.markdown("### Number of Incidents per Cluster")
+    fig2 = px.histogram(data_frame=df, x="new incidents", color="clusters")
+    st.plotly_chart(fig2)
 
-with fig_col3:
-    st.markdown("### Third Chart")
-    fig3 = px.histogram(data_frame=df, x="availability")
-    st.write(fig3) 
+# Display the fifth chart (Memory and Storage per Cluster)
+st.markdown("### Memory and Storage per Cluster")
+chart_data = pd.DataFrame({
+    'Clusters': df['clusters'],
+    'Metric': np.random.choice(['Memory', 'Storage'], size=len(df)),
+    'Value': np.random.randint(0, 100, size=len(df))
+})
 
+c = alt.Chart(chart_data).mark_bar().encode(
+    x='Clusters',
+    y='Value',
+    color='Metric',
+    tooltip=['Clusters', 'Metric', 'Value']
+).interactive()
+
+st.altair_chart(c, use_container_width=True)
 
